74695f029c10f550c6dd54c1dfe00db26c21909b
theo.gigant <giganttheo.com>
2023-04-21 17:05:34 +0200

    wip

--- wip.py
+++ wip.py
@@ -208,6 +208,7 @@ class FlaxTextToVideoControlNetPipeline(FlaxDiffusionPipeline):
                 # te = text_embeddings
                 # te = jnp.concatenate([repeat(text_embeddings[0, :, :], "c k -> f c k", f=f),
                 #    repeat(text_embeddings[-1, :, :], "c k -> f c k", f=f)])
+                f = latents.shape[0]
                 te = jnp.stack([text_embeddings[0, :, :]]*f + [text_embeddings[-1,:,:]*f])
                 
                 print(latents_local.shape, text_embeddings.shape, te.shape)
