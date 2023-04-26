519222b76365e91bc1b6f79732e8d15cca650881
theo.gigant <giganttheo.com>
2023-04-26 15:04:29 +0200

    testing no parallel

--- flax_text_to_video_pipeline.py
+++ flax_text_to_video_pipeline.py
@@ -189,7 +189,9 @@ class FlaxTextToVideoPipeline(FlaxDiffusionPipeline):
                 x_t0_1 = jax.lax.select(step < len(timesteps)-1 and timesteps[step+1] == t0, latents, x_t0_1)
                 x_t1_1 = jax.lax.select(step < len(timesteps)-1 and timesteps[step+1] == t1, latents, x_t1_1)
                 return (latents, x_t0_1, x_t1_1, scheduler_state)
-            (latents, x_t0_1, x_t1_1, scheduler_state) = jax.lax.cond(t > skip_t, lambda val:val, continue_loop, (latents, x_t0_1, x_t1_1, scheduler_state))
+            if t > skip_t:
+                (latents, x_t0_1, x_t1_1, scheduler_state) = continue_loop(latents, x_t0_1, x_t1_1, scheduler_state)
+            #(latents, x_t0_1, x_t1_1, scheduler_state) = jax.lax.cond(t > skip_t, lambda val:val, continue_loop, (latents, x_t0_1, x_t1_1, scheduler_state))
             return (latents, x_t0_1, x_t1_1, scheduler_state)
         latents_shape = latents.shape
         x_t0_1, x_t1_1 = jnp.zeros(latents_shape), jnp.zeros(latents_shape)
