879293bc2bf46d2f3dce216c5ae2cabc2876780e
theo.gigant <giganttheo.com>
2023-04-27 13:03:46 +0200

    requirements

--- flax_text_to_video_pipeline.py
+++ flax_text_to_video_pipeline.py
@@ -315,11 +315,14 @@ class FlaxTextToVideoPipeline(FlaxDiffusionPipeline):
         # backward stepts by stable diffusion
 
         #TODO: if a controlnet video is provided, it might need to be warped wrt refence_flow as well
-        controlet_image_warped = self.warp_latents_independently(controlnet_image[None], reference_flow)[0]
+        for idx, c_img in enumerate(controlnet_image):
+            controlnet_image = controlnet_image.at[idx].set(self.warp_latents_independently(
+                c_img[None], reference_flow)[0])
+
 
         ddim_res = self.DDIM_backward(params, num_inference_steps=num_inference_steps, timesteps=timesteps, skip_t=t1, t0=-1, t1=-1, do_classifier_free_guidance=do_classifier_free_guidance,
                                             text_embeddings=text_embeddings, latents_local=x_t1, guidance_scale=guidance_scale,
-                                            controlnet_image=controlet_image_warped, controlnet_conditioning_scale=controlnet_conditioning_scale)
+                                            controlnet_image=controlnet_image, controlnet_conditioning_scale=controlnet_conditioning_scale)
         x0 = ddim_res["x0"]
         return x0
 
