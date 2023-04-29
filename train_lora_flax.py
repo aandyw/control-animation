53253e889111a6501343a1719e39e37dab1f9929
theo.gigant <giganttheo.com>
2023-04-29 22:54:19 +0200

    debug lora

--- train_lora_flax.py
+++ train_lora_flax.py
@@ -165,104 +165,104 @@ def parse_args():
     return args
 
 
-class DreamBoothDataset(Dataset):
-    """
-    A dataset to prepare the instance and class images with the prompts for fine-tuning the model.
-    It pre-processes the images and the tokenizes prompts.
-    """
+# class DreamBoothDataset(Dataset):
+#     """
+#     A dataset to prepare the instance and class images with the prompts for fine-tuning the model.
+#     It pre-processes the images and the tokenizes prompts.
+#     """
 
-    def __init__(
-        self,
-        instance_data_root,
-        instance_prompt,
-        tokenizer,
-        class_data_root=None,
-        class_prompt=None,
-        class_num=None,
-        size=512,
-        center_crop=False,
-    ):
-        self.size = size
-        self.center_crop = center_crop
-        self.tokenizer = tokenizer
+#     def __init__(
+#         self,
+#         instance_data_root,
+#         instance_prompt,
+#         tokenizer,
+#         class_data_root=None,
+#         class_prompt=None,
+#         class_num=None,
+#         size=512,
+#         center_crop=False,
+#     ):
+#         self.size = size
+#         self.center_crop = center_crop
+#         self.tokenizer = tokenizer
 
-        self.instance_data_root = Path(instance_data_root)
-        if not self.instance_data_root.exists():
-            raise ValueError("Instance images root doesn't exists.")
+#         self.instance_data_root = Path(instance_data_root)
+#         if not self.instance_data_root.exists():
+#             raise ValueError("Instance images root doesn't exists.")
 
-        self.instance_images_path = list(Path(instance_data_root).iterdir())
-        self.num_instance_images = len(self.instance_images_path)
-        self.instance_prompt = instance_prompt
-        self._length = self.num_instance_images
+#         self.instance_images_path = list(Path(instance_data_root).iterdir())
+#         self.num_instance_images = len(self.instance_images_path)
+#         self.instance_prompt = instance_prompt
+#         self._length = self.num_instance_images
 
-        if class_data_root is not None:
-            self.class_data_root = Path(class_data_root)
-            self.class_data_root.mkdir(parents=True, exist_ok=True)
-            self.class_images_path = list(self.class_data_root.iterdir())
-            if class_num is not None:
-                self.num_class_images = min(len(self.class_images_path), class_num)
-            else:
-                self.num_class_images = len(self.class_images_path)
-            self._length = max(self.num_class_images, self.num_instance_images)
-            self.class_prompt = class_prompt
-        else:
-            self.class_data_root = None
+#         if class_data_root is not None:
+#             self.class_data_root = Path(class_data_root)
+#             self.class_data_root.mkdir(parents=True, exist_ok=True)
+#             self.class_images_path = list(self.class_data_root.iterdir())
+#             if class_num is not None:
+#                 self.num_class_images = min(len(self.class_images_path), class_num)
+#             else:
+#                 self.num_class_images = len(self.class_images_path)
+#             self._length = max(self.num_class_images, self.num_instance_images)
+#             self.class_prompt = class_prompt
+#         else:
+#             self.class_data_root = None
 
-        self.image_transforms = transforms.Compose(
-            [
-                transforms.Resize(size, interpolation=transforms.InterpolationMode.BILINEAR),
-                transforms.CenterCrop(size) if center_crop else transforms.RandomCrop(size),
-                transforms.ToTensor(),
-                transforms.Normalize([0.5], [0.5]),
-            ]
-        )
+#         self.image_transforms = transforms.Compose(
+#             [
+#                 transforms.Resize(size, interpolation=transforms.InterpolationMode.BILINEAR),
+#                 transforms.CenterCrop(size) if center_crop else transforms.RandomCrop(size),
+#                 transforms.ToTensor(),
+#                 transforms.Normalize([0.5], [0.5]),
+#             ]
+#         )
 
-    def __len__(self):
-        return self._length
+#     def __len__(self):
+#         return self._length
 
-    def __getitem__(self, index):
-        example = {}
-        instance_image = Image.open(self.instance_images_path[index % self.num_instance_images])
-        if not instance_image.mode == "RGB":
-            instance_image = instance_image.convert("RGB")
-        example["instance_images"] = self.image_transforms(instance_image)
-        example["instance_prompt_ids"] = self.tokenizer(
-            self.instance_prompt,
-            padding="do_not_pad",
-            truncation=True,
-            max_length=self.tokenizer.model_max_length,
-        ).input_ids
+#     def __getitem__(self, index):
+#         example = {}
+#         instance_image = Image.open(self.instance_images_path[index % self.num_instance_images])
+#         if not instance_image.mode == "RGB":
+#             instance_image = instance_image.convert("RGB")
+#         example["instance_images"] = self.image_transforms(instance_image)
+#         example["instance_prompt_ids"] = self.tokenizer(
+#             self.instance_prompt,
+#             padding="do_not_pad",
+#             truncation=True,
+#             max_length=self.tokenizer.model_max_length,
+#         ).input_ids
 
-        if self.class_data_root:
-            class_image = Image.open(self.class_images_path[index % self.num_class_images])
-            if not class_image.mode == "RGB":
-                class_image = class_image.convert("RGB")
-            example["class_images"] = self.image_transforms(class_image)
-            example["class_prompt_ids"] = self.tokenizer(
-                self.class_prompt,
-                padding="do_not_pad",
-                truncation=True,
-                max_length=self.tokenizer.model_max_length,
-            ).input_ids
+#         if self.class_data_root:
+#             class_image = Image.open(self.class_images_path[index % self.num_class_images])
+#             if not class_image.mode == "RGB":
+#                 class_image = class_image.convert("RGB")
+#             example["class_images"] = self.image_transforms(class_image)
+#             example["class_prompt_ids"] = self.tokenizer(
+#                 self.class_prompt,
+#                 padding="do_not_pad",
+#                 truncation=True,
+#                 max_length=self.tokenizer.model_max_length,
+#             ).input_ids
 
-        return example
+#         return example
 
 
-class PromptDataset(Dataset):
-    "A simple dataset to prepare the prompts to generate class images on multiple GPUs."
+# class PromptDataset(Dataset):
+#     "A simple dataset to prepare the prompts to generate class images on multiple GPUs."
 
-    def __init__(self, prompt, num_samples):
-        self.prompt = prompt
-        self.num_samples = num_samples
+#     def __init__(self, prompt, num_samples):
+#         self.prompt = prompt
+#         self.num_samples = num_samples
 
-    def __len__(self):
-        return self.num_samples
+#     def __len__(self):
+#         return self.num_samples
 
-    def __getitem__(self, index):
-        example = {}
-        example["prompt"] = self.prompt
-        example["index"] = index
-        return example
+#     def __getitem__(self, index):
+#         example = {}
+#         example["prompt"] = self.prompt
+#         example["index"] = index
+#         return example
 
 
 def get_full_repo_name(model_id: str, organization: Optional[str] = None, token: Optional[str] = None):
@@ -382,18 +382,24 @@ def main():
         #examples should have batch_size of jax.device_count()
         #result is already sharded
         size = 512
-        input_ids = [[transforms.Compose(
+        
+        input_ids = [tokenizer(
+                        example,
+                        padding="do_not_pad",
+                        truncation=True,
+                        max_length=tokenizer.model_max_length,
+                    ).input_ids for list_prompts in examples["caption"] for example in list_prompts]
+        
+        topil = [Image.fromarray(np.array(frame, dtype="uint8")) for i in range(len(examples)) for frame in d[i]['frames']]
+
+        pixel_values = [[transforms.Compose(
             [
                 transforms.Resize(size, interpolation=transforms.InterpolationMode.BILINEAR),
                 transforms.RandomCrop(size),
                 transforms.ToTensor(),
                 transforms.Normalize([0.5], [0.5]),
-            ])
-            (example["instance_prompt_ids"]) for example in examples[i]["caption"]]
-            for i in range(len(examples))] 
-        
-        pixel_values = [[example["instance_images"] for example in examples[i]["frames"]]
-            for i in range(len(examples))]
+            ])(example) for example in topil[i]]
+            for i in range(len(topil))] 
 
         pixel_values = torch.stack(pixel_values)
         pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
