5a352b187ad72a372ece6303e66e3907be91c339
theo.gigant <giganttheo.com>
2023-05-05 01:06:54 +0200

    lora model test

--- text_to_animation/pipelines/text_to_video_pipeline_flax.py
+++ text_to_animation/pipelines/text_to_video_pipeline_flax.py
@@ -531,7 +531,9 @@ class FlaxTextToVideoPipeline(FlaxDiffusionPipeline):
         controlnet_image = jnp.stack([controlnet_image[0]] * 2)
         #latent is shape # b c h w
         vmap_gen_start_frame = jax.vmap(lambda latent: self._generate_starting_frames(num_inference_steps, params, timesteps, text_embeddings, latent[None], guidance_scale, controlnet_image, controlnet_conditioning_scale))
-        return vmap_gen_start_frame(latents)
+        decoded_latents = vmap_gen_start_frame(latents)
+        print(f"shape output: {decoded_latents.shape}")
+        return decoded_latents
 
     def generate_video(
         self,
