016109ff9e69f74c5bbd3775d1028f87c1100c19
theo.gigant <giganttheo.com>
2023-05-08 01:22:23 +0200

    lora scale

--- text_to_animation/model.py
+++ text_to_animation/model.py
@@ -59,48 +59,45 @@ class ControlAnimationModel:
 
     def set_model(
         self,
-        # model_type: ModelType,
         model_id: str,
-        controlnet,
-        controlnet_params,
-        tokenizer,
-        scheduler,
-        scheduler_state,
         **kwargs,
     ):
         if hasattr(self, "pipe") and self.pipe is not None:
             del self.pipe
             self.pipe = None
         gc.collect()
-        model_id = "gigant/lora-aardman-sd15"
-        textual_inversion_id="gigant/textual_inversion_lora_aardman"
+
+        controlnet, controlnet_params = FlaxControlNetModel.from_pretrained(
+            "fusing/stable-diffusion-v1-5-controlnet-openpose",
+            from_pt=True,
+            dtype=jnp.float16,
+        )
+
         scheduler, scheduler_state = FlaxDDIMScheduler.from_pretrained(
-            model_id, subfolder="scheduler"
+            model_id, subfolder="scheduler", from_pt=True
         )
-        tokenizer = CLIPTokenizer.from_pretrained(
-            textual_inversion_id, subfolder="tokenizer" #model is loaded from the textual inversion checkpoint, finetuned for stop motion style
-        )
+        tokenizer = CLIPTokenizer.from_pretrained(model_id, subfolder="tokenizer")
         feature_extractor = CLIPFeatureExtractor.from_pretrained(
             model_id, subfolder="feature_extractor"
         )
-        unet, unet_params = FlaxLoRAUNet2DConditionModel.from_pretrained(
-            model_id, subfolder="unet", dtype=self.dtype
+        unet, unet_params = FlaxLoraUNet2DConditionModel.from_pretrained(
+            model_id, subfolder="unet", from_pt=True, dtype=self.dtype
         )
-        # unet_vanilla = UNet2DConditionModel.from_config(
-        #     model_id, subfolder="unet", dtype=self.dtype
-        # )
+        unet_vanilla = FlaxLoraUNet2DConditionModel.from_config(
+            model_id, subfolder="unet", from_pt=True, dtype=self.dtype
+        )
         vae, vae_params = FlaxAutoencoderKL.from_pretrained(
-            model_id, subfolder="vae", dtype=self.dtype
+            model_id, subfolder="vae", from_pt=True, dtype=self.dtype
         )
         text_encoder = FlaxCLIPTextModel.from_pretrained(
-            textual_inversion_id, subfolder="text_encoder", dtype=self.dtype
+            model_id, subfolder="text_encoder", from_pt=True, dtype=self.dtype
         )
         self.pipe = FlaxTextToVideoPipeline(
             vae=vae,
             text_encoder=text_encoder,
             tokenizer=tokenizer,
             unet=unet,
-            unet_vanilla=unet,
+            unet_vanilla=unet_vanilla,
             controlnet=controlnet,
             scheduler=scheduler,
             safety_checker=None,
@@ -114,9 +111,7 @@ class ControlAnimationModel:
             "text_encoder": text_encoder.params,
         }
         self.p_params = jax_utils.replicate(self.params)
-
         self.model_name = model_id
-
     def inference(self, image, split_to_chunks=False, chunk_size=8, **kwargs):
         if not hasattr(self, "pipe") or self.pipe is None:
             return
@@ -269,194 +264,194 @@ class ControlAnimationModel:
         return utils.create_gif(np.array(vid), 4, path=None, watermark=None)
         
 
-    def process_controlnet_pose(
-        self,
-        video_path,
-        prompt,
-        chunk_size=8,
-        watermark="Picsart AI Research",
-        merging_ratio=0.0,
-        num_inference_steps=20,
-        controlnet_conditioning_scale=1.0,
-        guidance_scale=9.0,
-        seed=42,
-        eta=0.0,
-        resolution=512,
-        use_cf_attn=True,
-        save_path=None,
-    ):
-        print("Module Pose")
-        video_path = gradio_utils.motion_to_video_path(video_path)
-        if self.model_type != ModelType.ControlNetPose:
-            controlnet = FlaxControlNetModel.from_pretrained(
-                "fusing/stable-diffusion-v1-5-controlnet-openpose"
-            )
-            self.set_model(
-                ModelType.ControlNetPose,
-                model_id="runwayml/stable-diffusion-v1-5",
-                controlnet=controlnet,
-            )
-            self.pipe.scheduler = FlaxDDIMScheduler.from_config(
-                self.pipe.scheduler.config
-            )
-            if use_cf_attn:
-                self.pipe.unet.set_attn_processor(processor=self.controlnet_attn_proc)
-                self.pipe.controlnet.set_attn_processor(
-                    processor=self.controlnet_attn_proc
-                )
+    # def process_controlnet_pose(
+    #     self,
+    #     video_path,
+    #     prompt,
+    #     chunk_size=8,
+    #     watermark="Picsart AI Research",
+    #     merging_ratio=0.0,
+    #     num_inference_steps=20,
+    #     controlnet_conditioning_scale=1.0,
+    #     guidance_scale=9.0,
+    #     seed=42,
+    #     eta=0.0,
+    #     resolution=512,
+    #     use_cf_attn=True,
+    #     save_path=None,
+    # ):
+    #     print("Module Pose")
+    #     video_path = gradio_utils.motion_to_video_path(video_path)
+    #     if self.model_type != ModelType.ControlNetPose:
+    #         controlnet = FlaxControlNetModel.from_pretrained(
+    #             "fusing/stable-diffusion-v1-5-controlnet-openpose"
+    #         )
+    #         self.set_model(
+    #             ModelType.ControlNetPose,
+    #             model_id="runwayml/stable-diffusion-v1-5",
+    #             controlnet=controlnet,
+    #         )
+    #         self.pipe.scheduler = FlaxDDIMScheduler.from_config(
+    #             self.pipe.scheduler.config
+    #         )
+    #         if use_cf_attn:
+    #             self.pipe.unet.set_attn_processor(processor=self.controlnet_attn_proc)
+    #             self.pipe.controlnet.set_attn_processor(
+    #                 processor=self.controlnet_attn_proc
+    #             )
 
-        video_path = (
-            gradio_utils.motion_to_video_path(video_path)
-            if "Motion" in video_path
-            else video_path
-        )
+    #     video_path = (
+    #         gradio_utils.motion_to_video_path(video_path)
+    #         if "Motion" in video_path
+    #         else video_path
+    #     )
 
-        added_prompt = "best quality, extremely detailed, HD, ultra-realistic, 8K, HQ, masterpiece, trending on artstation, art, smooth"
-        negative_prompts = "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer difits, cropped, worst quality, low quality, deformed body, bloated, ugly, unrealistic"
+    #     added_prompt = "best quality, extremely detailed, HD, ultra-realistic, 8K, HQ, masterpiece, trending on artstation, art, smooth"
+    #     negative_prompts = "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer difits, cropped, worst quality, low quality, deformed body, bloated, ugly, unrealistic"
 
-        video, fps = utils.prepare_video(
-            video_path, resolution, self.device, self.dtype, False, output_fps=4
-        )
-        control = (
-            utils.pre_process_pose(video, apply_pose_detect=False)
-            .to(self.device)
-            .to(self.dtype)
-        )
-        f, _, h, w = video.shape
-        self.generator.manual_seed(seed)
-        latents = torch.randn(
-            (1, 4, h // 8, w // 8),
-            dtype=self.dtype,
-            device=self.device,
-            generator=self.generator,
-        )
-        latents = latents.repeat(f, 1, 1, 1)
-        result = self.inference(
-            image=control,
-            prompt=prompt + ", " + added_prompt,
-            height=h,
-            width=w,
-            negative_prompt=negative_prompts,
-            num_inference_steps=num_inference_steps,
-            guidance_scale=guidance_scale,
-            controlnet_conditioning_scale=controlnet_conditioning_scale,
-            eta=eta,
-            latents=latents,
-            seed=seed,
-            output_type="numpy",
-            split_to_chunks=True,
-            chunk_size=chunk_size,
-            merging_ratio=merging_ratio,
-        )
-        return utils.create_gif(
-            result,
-            fps,
-            path=save_path,
-            watermark=gradio_utils.logo_name_to_path(watermark),
-        )
+    #     video, fps = utils.prepare_video(
+    #         video_path, resolution, self.device, self.dtype, False, output_fps=4
+    #     )
+    #     control = (
+    #         utils.pre_process_pose(video, apply_pose_detect=False)
+    #         .to(self.device)
+    #         .to(self.dtype)
+    #     )
+    #     f, _, h, w = video.shape
+    #     self.generator.manual_seed(seed)
+    #     latents = torch.randn(
+    #         (1, 4, h // 8, w // 8),
+    #         dtype=self.dtype,
+    #         device=self.device,
+    #         generator=self.generator,
+    #     )
+    #     latents = latents.repeat(f, 1, 1, 1)
+    #     result = self.inference(
+    #         image=control,
+    #         prompt=prompt + ", " + added_prompt,
+    #         height=h,
+    #         width=w,
+    #         negative_prompt=negative_prompts,
+    #         num_inference_steps=num_inference_steps,
+    #         guidance_scale=guidance_scale,
+    #         controlnet_conditioning_scale=controlnet_conditioning_scale,
+    #         eta=eta,
+    #         latents=latents,
+    #         seed=seed,
+    #         output_type="numpy",
+    #         split_to_chunks=True,
+    #         chunk_size=chunk_size,
+    #         merging_ratio=merging_ratio,
+    #     )
+    #     return utils.create_gif(
+    #         result,
+    #         fps,
+    #         path=save_path,
+    #         watermark=gradio_utils.logo_name_to_path(watermark),
+    #     )
 
-    def process_text2video(
-        self,
-        prompt,
-        model_name="dreamlike-art/dreamlike-photoreal-2.0",
-        motion_field_strength_x=12,
-        motion_field_strength_y=12,
-        t0=44,
-        t1=47,
-        n_prompt="",
-        chunk_size=8,
-        video_length=8,
-        watermark="Picsart AI Research",
-        merging_ratio=0.0,
-        seed=0,
-        resolution=512,
-        fps=2,
-        use_cf_attn=True,
-        use_motion_field=True,
-        smooth_bg=False,
-        smooth_bg_strength=0.4,
-        path=None,
-    ):
-        print("Module Text2Video")
-        if self.model_type != ModelType.Text2Video or model_name != self.model_name:
-            print("Model update")
-            unet = FlaxUNet2DConditionModel.from_pretrained(
-                model_name, subfolder="unet"
-            )
-            self.set_model(ModelType.Text2Video, model_id=model_name, unet=unet)
-            self.pipe.scheduler = FlaxDDIMScheduler.from_config(
-                self.pipe.scheduler.config
-            )
-            if use_cf_attn:
-                self.pipe.unet.set_attn_processor(processor=self.text2video_attn_proc)
-        self.generator.manual_seed(seed)
+    # def process_text2video(
+    #     self,
+    #     prompt,
+    #     model_name="dreamlike-art/dreamlike-photoreal-2.0",
+    #     motion_field_strength_x=12,
+    #     motion_field_strength_y=12,
+    #     t0=44,
+    #     t1=47,
+    #     n_prompt="",
+    #     chunk_size=8,
+    #     video_length=8,
+    #     watermark="Picsart AI Research",
+    #     merging_ratio=0.0,
+    #     seed=0,
+    #     resolution=512,
+    #     fps=2,
+    #     use_cf_attn=True,
+    #     use_motion_field=True,
+    #     smooth_bg=False,
+    #     smooth_bg_strength=0.4,
+    #     path=None,
+    # ):
+    #     print("Module Text2Video")
+    #     if self.model_type != ModelType.Text2Video or model_name != self.model_name:
+    #         print("Model update")
+    #         unet = FlaxUNet2DConditionModel.from_pretrained(
+    #             model_name, subfolder="unet"
+    #         )
+    #         self.set_model(ModelType.Text2Video, model_id=model_name, unet=unet)
+    #         self.pipe.scheduler = FlaxDDIMScheduler.from_config(
+    #             self.pipe.scheduler.config
+    #         )
+    #         if use_cf_attn:
+    #             self.pipe.unet.set_attn_processor(processor=self.text2video_attn_proc)
+    #     self.generator.manual_seed(seed)
 
-        added_prompt = "high quality, HD, 8K, trending on artstation, high focus, dramatic lighting"
-        negative_prompts = "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer difits, cropped, worst quality, low quality, deformed body, bloated, ugly, unrealistic"
+    #     added_prompt = "high quality, HD, 8K, trending on artstation, high focus, dramatic lighting"
+    #     negative_prompts = "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer difits, cropped, worst quality, low quality, deformed body, bloated, ugly, unrealistic"
 
-        prompt = prompt.rstrip()
-        if len(prompt) > 0 and (prompt[-1] == "," or prompt[-1] == "."):
-            prompt = prompt.rstrip()[:-1]
-        prompt = prompt.rstrip()
-        prompt = prompt + ", " + added_prompt
-        if len(n_prompt) > 0:
-            negative_prompt = n_prompt
-        else:
-            negative_prompt = None
+    #     prompt = prompt.rstrip()
+    #     if len(prompt) > 0 and (prompt[-1] == "," or prompt[-1] == "."):
+    #         prompt = prompt.rstrip()[:-1]
+    #     prompt = prompt.rstrip()
+    #     prompt = prompt + ", " + added_prompt
+    #     if len(n_prompt) > 0:
+    #         negative_prompt = n_prompt
+    #     else:
+    #         negative_prompt = None
 
-        result = self.inference(
-            prompt=prompt,
-            video_length=video_length,
-            height=resolution,
-            width=resolution,
-            num_inference_steps=50,
-            guidance_scale=7.5,
-            guidance_stop_step=1.0,
-            t0=t0,
-            t1=t1,
-            motion_field_strength_x=motion_field_strength_x,
-            motion_field_strength_y=motion_field_strength_y,
-            use_motion_field=use_motion_field,
-            smooth_bg=smooth_bg,
-            smooth_bg_strength=smooth_bg_strength,
-            seed=seed,
-            output_type="numpy",
-            negative_prompt=negative_prompt,
-            merging_ratio=merging_ratio,
-            split_to_chunks=True,
-            chunk_size=chunk_size,
-        )
-        return utils.create_video(
-            result, fps, path=path, watermark=gradio_utils.logo_name_to_path(watermark)
-        )
+    #     result = self.inference(
+    #         prompt=prompt,
+    #         video_length=video_length,
+    #         height=resolution,
+    #         width=resolution,
+    #         num_inference_steps=50,
+    #         guidance_scale=7.5,
+    #         guidance_stop_step=1.0,
+    #         t0=t0,
+    #         t1=t1,
+    #         motion_field_strength_x=motion_field_strength_x,
+    #         motion_field_strength_y=motion_field_strength_y,
+    #         use_motion_field=use_motion_field,
+    #         smooth_bg=smooth_bg,
+    #         smooth_bg_strength=smooth_bg_strength,
+    #         seed=seed,
+    #         output_type="numpy",
+    #         negative_prompt=negative_prompt,
+    #         merging_ratio=merging_ratio,
+    #         split_to_chunks=True,
+    #         chunk_size=chunk_size,
+    #     )
+    #     return utils.create_video(
+    #         result, fps, path=path, watermark=gradio_utils.logo_name_to_path(watermark)
+    #     )
 
-    def generate_animation(
-        self,
-        prompt: str,
-        model_link: str = "dreamlike-art/dreamlike-photoreal-2.0",
-        is_safetensor: bool = False,
-        motion_field_strength_x: int = 12,
-        motion_field_strength_y: int = 12,
-        t0: int = 44,
-        t1: int = 47,
-        n_prompt: str = "",
-        chunk_size: int = 8,
-        video_length: int = 8,
-        merging_ratio: float = 0.0,
-        seed: int = 0,
-        resolution: int = 512,
-        fps: int = 2,
-        use_cf_attn: bool = True,
-        use_motion_field: bool = True,
-        smooth_bg: bool = False,
-        smooth_bg_strength: float = 0.4,
-        path: str = None,
-    ):
-        if is_safetensor and model_link[-len(".safetensors") :] == ".safetensors":
-            pipe = utils.load_safetensors_model(model_link)
-        return
+    # def generate_animation(
+    #     self,
+    #     prompt: str,
+    #     model_link: str = "dreamlike-art/dreamlike-photoreal-2.0",
+    #     is_safetensor: bool = False,
+    #     motion_field_strength_x: int = 12,
+    #     motion_field_strength_y: int = 12,
+    #     t0: int = 44,
+    #     t1: int = 47,
+    #     n_prompt: str = "",
+    #     chunk_size: int = 8,
+    #     video_length: int = 8,
+    #     merging_ratio: float = 0.0,
+    #     seed: int = 0,
+    #     resolution: int = 512,
+    #     fps: int = 2,
+    #     use_cf_attn: bool = True,
+    #     use_motion_field: bool = True,
+    #     smooth_bg: bool = False,
+    #     smooth_bg_strength: float = 0.4,
+    #     path: str = None,
+    # ):
+    #     if is_safetensor and model_link[-len(".safetensors") :] == ".safetensors":
+    #         pipe = utils.load_safetensors_model(model_link)
+    #     return
 
-    # def generate_initial_frames(
+    # # def generate_initial_frames(
     #     self,
     #     prompt: str,
     #     model_link: str = "dreamlike-art/dreamlike-photoreal-2.0",
